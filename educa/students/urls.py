b8e1ffb91a071195094490a38846fcb8a1a34e5b
acascell <infocascella@gmail.com>
2024-05-09 14:25:17 +0200

    feat: defined students app
    - created student app, will be user for the user to interact with the courses, register etc...
    - created registration view extending CreateView
    - updated main urls router and student url
    - defined registration html template

---
+++ educa/students/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path(
+        "register/",
+        views.StudentRegistrationView.as_view(),
+        name="student_registration",
+    ),
+]
