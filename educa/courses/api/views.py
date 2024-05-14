c540028bb37f6fa749c3f4abc350ffbc0e9168db
acascell <infocascella@gmail.com>
2024-05-14 12:51:46 +0200

    feat: defined enroll view
    - created enroll view to allow users to register to courses using built-in django auth (will enhance this part)

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,11 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
-from courses.models import Subject
-from courses.api.serializers import SubjectSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from courses.models import Subject, Course
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,3 +16,13 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({"enrolled": True})
