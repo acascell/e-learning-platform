07274a333ae49859a9da24278c5d2c2eb9d4feb9
acascell <infocascella@gmail.com>
2024-05-14 12:45:57 +0200

    feat: implemented api views using rest framework
    - included multiple generics views to list and retrieve data via api call for both subjects and courses
    - updated settings, requirements, urls router
    - defined multiple model serializers for the defined models

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "embed_video",
     "debug_toolbar",
     "redisboard",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,9 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
