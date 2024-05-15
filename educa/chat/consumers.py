6363549a86bc888bb02c08d034b620243df1a454
acascell <infocascella@gmail.com>
2024-05-15 11:19:35 +0200

    feat: implemented asynch message relay to multiple instance of the chat app

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,18 +1,35 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.room_group_name = f"chat_{self.id}"
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
         # accept connection
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     # receive message from WebSocket
     def receive(self, text_data):
+        # send message to room group
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        # send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    # receive a message from room group
+    def chat_message(self, event):
+        # send message to websocket
+        self.send(text_data=json.dumps(event))
