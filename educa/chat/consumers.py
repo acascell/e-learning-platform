080cd17264a293be3ed4aad410fc38a20ad40073
acascell <infocascella@gmail.com>
2024-05-15 11:38:59 +0200

    feat: included username, datetime and minor features
    - now possible to display user (logic allow to display the current user or another user) and highlight with different colors on the chat
    - updated template and consumers

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,10 +1,12 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.id}"
         # join room group
@@ -26,7 +28,13 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "datetime": timezone.now().isoformat(),
+            },
         )
 
     # receive a message from room group
