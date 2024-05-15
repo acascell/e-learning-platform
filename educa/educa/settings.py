7bd5d80fc3e27b1ca8e1a8a47e521bf4d9a89ce4
acascell <infocascella@gmail.com>
2024-05-15 11:07:30 +0200

    feat: enabled redis channel layer for multiple instance communication

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -163,3 +163,13 @@ REST_FRAMEWORK = {
 }
 
 ASGI_APPLICATION = "educa.asgi.application"
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
